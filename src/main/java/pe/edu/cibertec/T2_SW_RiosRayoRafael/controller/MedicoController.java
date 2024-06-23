743e165f3d803f918adb4a4cf15f559b2894c6be
rafaelrsry <rafaelrsry@gmail.com>
2024-06-23 11:37:21 -0500

    Cuarto commit pregunta uno y dos

--- src/main/java/pe/edu/cibertec/T2_SW_RiosRayoRafael/controller/MedicoController.java
+++ src/main/java/pe/edu/cibertec/T2_SW_RiosRayoRafael/controller/MedicoController.java
@@ -1,4 +1,10 @@
 package pe.edu.cibertec.T2_SW_RiosRayoRafael.controller;
 
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
 public class MedicoController {
 }
--- src/main/java/pe/edu/cibertec/T2_SW_RiosRayoRafael/service/EspecialidadService.java
+++ src/main/java/pe/edu/cibertec/T2_SW_RiosRayoRafael/service/EspecialidadService.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 @Service
 public class EspecialidadService implements IEspecialidadService{
+
+
 }
